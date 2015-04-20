9721dfb392dd465a0603d3fe59c3f1eeee72e937
brtok <brtokarski@gmail.com>
2015-04-20 13:23:34 -0300

    novas classes

--- distrib1/src/distrib1/Distrib1.java
+++ distrib1/src/distrib1/Distrib1.java
@@ -5,6 +5,11 @@
  */
 package distrib1;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Bruno
@@ -15,7 +20,16 @@ public class Distrib1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Distrib1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        JanelaMinhaIDPublica jid = new JanelaMinhaIDPublica();
+        jid.setVisible(true);
+        jid.repaint();
     }
     
 }
