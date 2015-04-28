85aae57262d43acccc56154220b39332730c6b41
RTVidal <rafaelvidal4@gmail.com>
2015-04-28 17:23:03 -0300

    Resolvendo conflitos

--- distrib1/src/Comunicação/ComunicacaoRecebeInicial.java
+++ distrib1/src/Comunicação/ComunicacaoRecebeInicial.java
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Comunicação;
-
-import GUI.JanelaConsole;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.UnknownHostException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- *
- * @author Bruno
- */
-public class ComunicacaoRecebeInicial extends Thread {
-
-    /**
-     *
-     */
-    @Override
-    public void run() {
-        try {
-            boolean naotem4 = true;
-            InetAddress addres = InetAddress.getByName(DefinirComunicacao.INET_ADDR);
-            try (MulticastSocket clientSocket = new MulticastSocket(DefinirComunicacao.PORT)) {
-                clientSocket.joinGroup(addres);
-                while (naotem4) {
-                    byte[] buf = new byte[256];
-                    DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
-                    clientSocket.receive(msgPacket);
-                    String msg = new String(buf);
-                    Date now = new Date();
-                    DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                    String dh = formatter.format(now);
-                    JanelaConsole.escreveNaJanela(dh + " Recebeu: " + msg.trim());
-                }
-                clientSocket.leaveGroup(addres);
-            } catch (IOException ex) {
-                Logger.getLogger(ComunicacaoRecebeInicial.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(ComunicacaoRecebeInicial.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -121,4 +65,3 @@ public class ComunicacaoRecebeInicial extends Thread {
         }
     }
 }
->>>>>>> origin/master
