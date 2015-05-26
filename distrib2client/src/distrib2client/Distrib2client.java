4cb71f8f5a0365d7b8cd0abba4fb70c8a2adbfde
brtok <brtokarski@gmail.com>
2015-05-26 19:28:50 -0300

    tentando

--- distrib2client/src/distrib2client/Distrib2client.java
+++ distrib2client/src/distrib2client/Distrib2client.java
@@ -8,6 +8,8 @@ package distrib2client;
 import Comunicacao.RMICliente;
 import GUI.InicialClient;
 import GUI.JanelaConsole;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.UIManager;
@@ -21,8 +23,12 @@ public class Distrib2client {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.InterruptedException
      */
     public static void main(String[] args) throws InterruptedException {
+        Calendar gregoriano = new GregorianCalendar();
+        String print = gregoriano.get(Calendar.HOUR_OF_DAY) + ":" + gregoriano.get(Calendar.MINUTE) + ":00";
+        System.out.println(print);
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
@@ -41,7 +47,7 @@ public class Distrib2client {
         } catch (Exception ex) {
             Logger.getLogger(Distrib2client.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
 }
---
+++ distrib2server/src/Controle/ControleLocacao.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controle;
+
+import Modelo.Locacao;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Bruno
+ */
+public class ControleLocacao {
+ 
+    private static ControleLocacao instancia = new ControleLocacao();
+    private ArrayList<Locacao> locacoes = new ArrayList<>();
+
+    public static ControleLocacao getInstancia() {
+        return instancia;
+    }
+
+    public static void setInstancia(ControleLocacao instancia) {
+        ControleLocacao.instancia = instancia;
+    }
+    
+    public ArrayList<Locacao> getLocacoesPorVeiculo(int idVeiculo) {
+        ArrayList<Locacao> resultado = new ArrayList<>();
+        for (Locacao l : locacoes) {
+            if (l.getVeiculo().getIdVeiculo() == idVeiculo) {
+                resultado.add(l);
+            }
+        }
+        return resultado;
+    }
+    
+    public boolean verificaDisponibilidade(Date dataInicio, Time horaInicio, Date dataFim, Time horaFim, int idVeiculo) {
+        ArrayList<Locacao> locacoes = getLocacoesPorVeiculo(idVeiculo);
+        Date hoje = new Date();
+        
+    }
+    
+    public boolean addLocacao(Locacao l) {
+        boolean disponivel = verificaDisponibilidade(l.getDataRetirada(), l.getHoraRetirada(), l.getDataDevolucao(), l.getHoraDevolucao(), l.getVeiculo().getIdVeiculo());
+        if (disponivel) {
+            //CODIGO PARA ADICIONAR A LOCACAO
+            JOptionPane.showMessageDialog(null, "Locação solicitada com sucesso para o veículo.");
+            return true;
+        } else {
+            //CODIGO QUE CONFIRMA QUE NÃO ESTÁ DISPONÍVEL
+            JOptionPane.showMessageDialog(null, "O veículo já está locado no período solicitado.");
+            return false;
+        }
+    }
+
+}
