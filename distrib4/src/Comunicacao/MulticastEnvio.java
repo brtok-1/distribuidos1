e323e39456827ec0c4ce8ae74bd8b345728c5319
brtok <brtokarski@gmail.com>
2015-07-06 20:31:02 -0300

    commit

---
+++ distrib4/src/Comunicacao/MulticastEnvio.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comunicacao;
+
+import Modelo.Colecionador;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Bruno
+ */
+public class MulticastEnvio extends Thread {
+
+    /**
+     * Thread de envio de hello para clientes a fim de manter o status online
+     * frente aos mesmos
+     */
+    @Override
+    public void run() {
+        try {
+            DatagramSocket serverSocket = new DatagramSocket();
+            InetAddress address = InetAddress.getByName("228.5.6.7");
+            Colecionador instancia = Colecionador.getInstancia();
+            while (true) {
+                String mensagem = String.valueOf(instancia.getIdColecionador());
+                DatagramPacket msgPacket = new DatagramPacket(mensagem.getBytes(), mensagem.getBytes().length, address, 8885);
+                serverSocket.send(msgPacket);
+                sleep(8000);
+            }
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(MulticastEnvio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ distrib4/src/Comunicacao/MulticastRecebimento.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comunicacao;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Bruno
+ */
+public class MulticastRecebimento extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            InetAddress address = InetAddress.getByName("228.5.6.7");
+            MulticastSocket clientSocket = new MulticastSocket(8885);
+            clientSocket.joinGroup(address);
+            while (true) {
+                byte[] buf = new byte[256];
+                DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
+                clientSocket.receive(msgPacket);
+                String mensagem = new String(buf);
+                int recebido = Integer.parseInt(mensagem.trim());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(MulticastRecebimento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
